4cefe2dd7d446a3cc995603b1967bf1737cbab81
Glitch (hello-webpage) <none>
2020-04-05 20:20:23 +0000

    🚂💃 Checkpoint ./lib/sprite.js:11379418/3278 ./index.js:11379418/34 ./package.json:286695/33 ./lib/level.js:286695/215

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import createGame from "crtrdg-gameloop"
 import drawLevel from "./lib/level"
-import drawSprite, {animateSprite} from "./lib/sprite"
+import {animateSprite} from "./lib/sprite"
 import drawUI from "./lib/ui"
 
 const game = createGame()
@@ -10,8 +10,8 @@ game.canvas.width = 512
 
 game.on("draw", function(ctx, dt) {
   drawLevel(ctx)
-  animateSprite(ctx, "wizzard_m_idle_anim", 140, 340)
-  animateSprite(ctx, "ogre_idle_anim", 420, 340, true)
+  animateSprite(ctx, "wizzard_m_idle_anim", 140, 240)
+  animateSprite(ctx, "ogre_idle_anim", 320, 240, true)
   drawUI(ctx)
 })
 
