274714a37d469093d68311aff3b08cc2d6c9ed17
Glitch (hello-webpage) <none>
2020-04-05 13:48:37 +0000

    🏮🏥 Checkpoint ./index.js:286695/1200:11379418/2392 ./.glitch-assets:286695/343 ./tiles_list_v1.3.txt:286695/4966

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const createGame = require('crtrdg-gameloop')
 
 const game = createGame()
 
-const image = new Image()
+const tileset = new Image(512, 512); // Using optional size for image
+tileset.src = 'https://cdn.glitch.com/8804483f-7435-434d-ab8d-d8d811696a6a%2F0x72_DungeonTilesetII_v1.3.png?v=1586091258409'
+
 
 // ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
 
+
+
 game.on('draw', function (ctx, dt) {
-  console.log(dt)
-  ctx.beginPath();
-  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
-  ctx.stroke();
+  // ctx.drawImage(tileset, 0, 0, 16, 16, 0, 0, 16, 16)
+  ctx.fillStyle = "#222"
+  ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height)
+  ctx.drawImage(tileset, 16, 64, 16, 16, 0, 0, 16, 16)
 })
 
 
