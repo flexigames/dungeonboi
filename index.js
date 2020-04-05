4d048bcc5944451732f29b91c281109c3c976351
Glitch (hello-webpage) <none>
2020-04-05 13:37:36 +0000

    👱🚨 Checkpoint ./index.js:286695/401:11379418/443

--- index.js
+++ index.js
@@ -2,7 +2,16 @@ const createGame = require('crtrdg-gameloop')
 
 const game = createGame()
 
-game.on('draw', function (renderer, dt) {
+const image = new Image()
+
+// ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+
+game.on('draw', function (ctx, dt) {
   console.log(dt)
+  ctx.beginPath();
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  ctx.stroke();
 })
 
+
+game.start()
