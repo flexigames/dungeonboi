74db7da915462de6c5058e0eb5372f1724981fde
Glitch (hello-webpage) <none>
2020-04-05 20:42:30 +0000

    🎤😀 Checkpoint ./lib/sprite.js:11379418/3695 ./tiles_list_v1.3.txt:286695/1 ./index.js:11379418/704 ./level.txt:286695/16 ./lib/level.js:286695/727 ./lib/player.js:11379418/760

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ import createGame from "crtrdg-gameloop"
 import drawLevel from "./lib/level"
 import {animateSprite} from "./lib/sprite"
 import drawUI from "./lib/ui"
+import Player from './lib/Player'
 
 const game = createGame()
 
 game.canvas.height = 512
 game.canvas.width = 512
 
+const entities = [
+  new Player(140, 240)
+]
+
+
 game.on("draw", function(ctx, dt) {
   drawLevel(ctx)
-  animateSprite(ctx, "wizzard_m_idle_anim", 140, 240)
-  animateSprite(ctx, "ogre_idle_anim", 320, 240, true)
+  drawEntities(ctx)
   drawUI(ctx)
 })
 
 game.start()
+
+function drawEntities(ctx) {
+  entities.forEach(it => it.draw(ctx))
+}
