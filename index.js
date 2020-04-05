e65eed64351e74abe6acd701bbea4fe969b0a970
Glitch (hello-webpage) <none>
2020-04-05 20:08:28 +0000

    🙎👦 Checkpoint ./index.js:11379418/90 ./lib/sprite.js:11379418/479 ./tiles_list_v1.3.txt:286695/1:11379418/340

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ game.canvas.width = 512
 
 game.on("draw", function(ctx, dt) {
   drawLevel(ctx)
-  animateSprite(ctx, "wizzard_m_idle_anim", 140, 140)
-  animateSprite(ctx, "ogre_idle_anim", 100, 100)
+  animateSprite(ctx, "wizzard_m_idle_anim", 140, 340)
+  animateSprite(ctx, "ogre_idle_anim", 420, 340, true)
   drawUI(ctx)
 })
 
