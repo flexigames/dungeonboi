02359ccc8f3c4070cca2969c84d87065a441555f
Finn Pauls <mbp@pauls.dev>
2020-04-11 01:37:39 +0200

    Add second enemy type

--- entities/Enemy.js
+++ entities/Enemy.js
@@ -2,17 +2,30 @@ import { findEntities, createEntity } from "../lib/entities"
 import Character from "./Character"
 import Corpse from "./Corpse"
 import V from "../lib/vec2"
+import {sample} from 'lodash'
+
+const enemies = [
+  {sprites: "necromancer_idle_anim", maxHealth: 2, speed: 0.5, attackRadius: 10},
+  {sprites: "big_demon_idle_anim", maxHealth: 5, speed: 0.25, attackRadius: 16}
+]
 
 export default class Enemy extends Character {
-  constructor(x, y) {
+  constructor(x, y, opts = {}) {
     super(x, y, {
       speed: 0.5,
       maxHealth: 2,
       sprites: "necromancer_idle_anim",
+      ...opts
     })
+    const {attackRadius =  10} = opts
     this.tags = ["enemy"]
     this.followDistance = 150
     this.xpGain = 10
+    this.attackRadius = attackRadius
+  }
+
+  static createRandom(x, y) {
+    return new Enemy(x, y, sample(enemies))
   }
 
   onDeath() {
@@ -41,12 +54,11 @@ export default class Enemy extends Character {
 
   checkPlayerHit() {
     const player = findEntities("player")[0]
-    const HIT_RADIUS = 10
     const DAMAGE = 1
 
     if (
       player &&
-      this.pos.distance(player.pos) < HIT_RADIUS &&
+      this.pos.distance(player.pos) < this.attackRadius &&
       Date.now() > this.immuneUntil
     ) {
       player.takeHit(DAMAGE, this.pos)
