13203b2868755a7aa874f550d7a664d5a7dcf93b
Finn Pauls <mbp@pauls.dev>
2020-04-29 12:34:36 +0200

    Fix ratios

--- index.js
+++ index.js
@@ -18,11 +18,6 @@ import generateDungeon from "./lib/dungeon"
 import { random, compact } from "lodash"
 import crash from "./lib/crash"
 import WebFont from 'webfontloader'
-import Stats from 'stats.js'
-
-const stats = new Stats()
-stats.showPanel(0)
-document.body.appendChild( stats.dom );
 
 WebFont.load({
   google: {
@@ -132,7 +127,6 @@ function startPIXI() {
 
     function createGameLoop(player, hud) {
       return function gameLoop(dt) {
-        stats.begin()
         if (gameState === 'playing') {
           crash.check()
           controlPlayer(player)
@@ -146,7 +140,6 @@ function startPIXI() {
           }
           hud.update()
         }
-        stats.end()
       }
     }
 }
